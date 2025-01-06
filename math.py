b4be155dfadc6e8ee8d1d22211ab97113e874769
Kondla <ap10@cdc.gov>
2025-01-05 21:58:57 -0500

    update

--- math.py
+++ math.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
-
 a = 10
 b = 20
 c = 100
+d = 50
 print ("Add", a + b)
 print ("substract", b -a)
 print ("multiply", a * b)
 print ("division", b/a)
 print ("division", c/a)
+print("multiply" c/d)
